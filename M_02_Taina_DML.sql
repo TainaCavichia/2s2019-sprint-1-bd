78b3b07c3899c679f054a7a329e6de60280cd438
Tainá Cavichia Alves <46748080819@info.local>
2019-08-06 11:29:00 -0300

    gufos

---
+++ M_02_Taina_DML.sql
@@ -0,0 +1,23 @@
+USE M_Gufos
+
+INSERT INTO Usuarios(Nome, Email, Senha, Permissao)
+	VALUES ('Administrador', 'admin@admin.com', '123456', 'Administrador');
+
+
+INSERT INTO Usuarios(Nome, Email, Senha, Permissao)
+	VALUES ('Josias Cabele', 'jc@senai.com', '123456', 'Aluno');
+
+INSERT INTO Categorias(Nome)
+	VALUES ('Jogos'),('Meetup'),('Futebol');
+
+SELECT * FROM Categorias ORDER BY IdCategoria ASC;
+
+INSERT INTO Eventos(Titulo, Descricao, DataEvento, Ativo, Localizacao, IdCategoria)
+	VALUES ('Ping Pong', 'Campeonato redes contra dev', GETDATE(), 1, 'Alameda Barão de Limeira, 539', 1);
+
+SELECT * FROM Eventos ORDER BY IdEvento ASC;
+SELECT * FROM Presencas;
+SELECT * FROM Usuarios;
+
+INSERT INTO Presencas(IdEvento, IdUsuario)
+	VALUES (1,2)
