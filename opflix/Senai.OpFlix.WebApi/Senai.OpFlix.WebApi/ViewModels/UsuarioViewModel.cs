d1b7502b64f5f826da68133c5e73d9e806d282a0
Tainá Cavichia Alves <46748080819@info.local>
2019-09-10 11:12:27 -0300

    opa

---
+++ opflix/Senai.OpFlix.WebApi/Senai.OpFlix.WebApi/ViewModels/UsuarioViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Senai.OpFlix.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.OpFlix.WebApi.ViewModels
+{
+    public class UsuarioViewModel
+    {
+        [Required]
+        public int IdUsuario { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Cpf { get; set; }
+        public string Telefone { get; set; }
+        public int? IdTipoUsuario { get; set; }
+
+        public TipoUsuarios IdTipoUsuarioNavigation { get; set; }
+        public List<Favoritos> Favoritos { get; set; }
+    }
+}
